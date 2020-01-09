9a25fe5d3091e2d6ca42accc8367c18d026735cc
Ubuntu <ubuntu@ip-172-31-86-192.ec2.internal>
2020-01-09 16:51:02 +0000

    Using trix

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,4 +2,21 @@ class SubmissionsController < ApplicationController
     def new
         @submission = Submission.new
     end
+    def create
+        @submission = Submission.new
+        @submission.user_id = current_user
+        if @submission.save
+            flash[:success] = "The submission was successfully submitted."
+            redirect_to units_path
+        else 
+            flash[:success] = "Submission was not submitted correctly, please try again."
+            redirect_back(fallback_location: root_path)
+
+        end
+    end
+    
+    def show
+        @show = Submission.find(params[:id])
+    end
+
 end
