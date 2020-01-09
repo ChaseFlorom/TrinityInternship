fc90a70d29f32f4171b6cac8c7dcc4d2e52a73eb
Ubuntu <ubuntu@ip-172-31-86-192.ec2.internal>
2020-01-09 17:23:58 +0000

    form for submissions not working, saving so i can play around

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,8 +3,9 @@ class SubmissionsController < ApplicationController
         @submission = Submission.new
     end
     def create
-        @submission = Submission.new
+        @submission = Submission.new(submission_params)
         @submission.user_id = current_user
+        @submission.assignment_id = @assignment.id
         if @submission.save
             flash[:success] = "The submission was successfully submitted."
             redirect_to units_path
@@ -17,6 +18,13 @@ class SubmissionsController < ApplicationController
     
     def show
         @show = Submission.find(params[:id])
+    end
+    
+    
+    private
+    
+    def submission_params
+        params.require(:assignment).permit(:user_id, :assignment_id)
     end
 
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ApplicationRecord
+    has_rich_text :content
     belongs_to :user
     belongs_to :assignment
 end
