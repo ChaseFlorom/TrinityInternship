90dbeffa8f94c60dc1f663b7213cf3c9a89019b4
Ubuntu <ubuntu@ip-172-31-86-192.ec2.internal>
2020-02-04 19:45:13 +0000

    Updated navbar with student/teacher permissions

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,5 +1,20 @@
 class GradesController < ApplicationController
+      before_action :require_teacher
+
   def index
     @users = User.where(role: "student").paginate(page: params[:page], per_page: 50)
+  end
+  
+  
+  private
+    
+
+  def require_teacher
+      if current_user.role == "teacher" || current_user.admin == true
+      
+      else    
+          flash[:danger] = "You do not have the required permissions to do that."
+          redirect_to root_path
+      end
   end
 end
