d616061d27b8cb43f3a6c78660bcc38715b4ac59
Ubuntu <ubuntu@ip-172-31-86-192.ec2.internal>
2020-05-30 20:56:59 +0000

    PRE-PRODUCTION PHASE

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   has_many :assignments, :through => :units
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   
   
     def fullname
