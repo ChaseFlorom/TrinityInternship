6835fea354cbfd71c3d9b2399d8fe0080f7f5ddb
Ubuntu <ubuntu@ip-172-31-86-192.ec2.internal>
2020-06-13 18:48:43 +0000

    Edit comments

--- app/controllers/comments_submissions_controller.rb
+++ app/controllers/comments_submissions_controller.rb
@@ -14,6 +14,30 @@ class CommentsSubmissionsController < ApplicationController
         end
     end
     
+    def edit
+        @comment = CommentsSubmission.find(params[:id])
+        require_current(@comment)
+
+    end
+    
+    def update
+        
+        @comment = CommentsSubmission.find(params[:id])
+        @submission = @comment.submission
+        @assignment = @submission.assignment
+        if(if_comment_owner(@comment))
+            if @comment.update(creation_params)
+                flash[:success] = "The comment was successfully updated."
+                redirect_to assignment_path(@assignment)
+            else
+                render 'edit'
+            end  
+        else
+            flash[:success] = "The comment was not submitted correctly, please try again."
+            redirect_back(fallback_location: root_path)
+        end
+    end
+    
     def destroy
         @comment = CommentsSubmission.find(params[:id])
         if current_user.role == "teacher" || @comment.user_id == current_user.id
@@ -29,7 +53,23 @@ class CommentsSubmissionsController < ApplicationController
     
     private
     
+    def if_comment_owner(comment)
+       if(comment.user_id == current_user.id)
+            return true
+        else
+            return false
+       end
+    end
+    
     def creation_params
         params.require(:comments_submission).permit!
+    end
+    def require_current(comment)
+        if comment.user_id == current_user.id|| current_user.admin == true
+        
+        else    
+            redirect_to units_path
+            flash[:danger] = "You do not have the required permissions to do that."
+        end
     end
 end
