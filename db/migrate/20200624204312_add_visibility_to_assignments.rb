9d29c003ccd4159ade9e98228f02dd4d20453786
Ubuntu <ubuntu@ip-172-31-86-192.ec2.internal>
2020-06-24 21:38:28 +0000

    small file delete

--- db/migrate/20200624204312_add_visibility_to_assignments.rb
+++
@@ -1,7 +0,0 @@
-class AddVisibilityToAssignments < ActiveRecord::Migration[6.0]
-  def change
-    def change
-      add_column :assignments, :visibility, :string, default: "private"
-    end
-  end
-end
